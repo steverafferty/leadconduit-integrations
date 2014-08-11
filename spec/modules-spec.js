fb07d8d6a267a785a80608ec02bf7af208a4fc56
Alex Wolfe <alexkwolfe@gmail.com>
2014-08-11 12:49:35 -0500

    Added support for listing all modules.

---
+++ spec/modules-spec.js
@@ -0,0 +1,13 @@
+var assert = require('chai').assert,
+    integrations = require('../index');
+
+describe('Modules', function() {
+
+  it('should have valid id', function() {
+    for (var id in integrations.modules) {
+      assert.equal(integrations.modules[id].id, id);
+      assert.match(integrations.modules[id].id, /[-.]/);
+    }
+  });
+
+});
